4a1a2bd1f33778ab0d74e9eaea78f856628a4130
James David Morris <jamesdavidmorris@gmail.com>
2013-10-17 15:12:16 -0700

    working release

---
+++ responsivem.js
@@ -0,0 +1,23 @@
+//include after jquery, and you need <!doctype html>, and use em for everything (including fonts) instead of px
+var rtime=new Date()-1000,resizetimeout=false,resizedelta=60,resizeemx=100,resizeemy=100;
+
+$(window).resize(function(){
+  rtime=new Date();
+  if(resizetimeout==false){
+    resizetimeout=true;setTimeout(resizeend,resizedelta);}}
+).resize(); 
+
+function resizeend(){
+  if(new Date()-rtime<resizedelta)
+    setTimeout(resizeend,resizedelta);
+  else{
+    resizetimeout = false;
+    var wx=$(window).width(),wy=$(window).height();
+    $('body').css('font-size',wx/wy>=resizeemx/resizeemy?wy/resizeemy:wx/resizeemx);
+  }
+}
+
+//override default minimum width and height in em. If your app is 16:9 you might use responsivem(160,90); and your main div should be 160em by 90em
+function responsivem(xem,yem){
+  resizeemx=xem;resizeemy=yem;$(window).resize();
+}
